d40512a01379aa23470b01e92152b334629cb176
Franco-Serra <frankmourice28@gmail.com>
2025-07-15 03:19:53 -0300

    cruds implementados

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,5 @@
 const Product = require('../models/product');
 
-
 const getProductAll = async (req, res) => { 
     const codigo = req.query.codigo;
     const query = !codigo ? {} : { codigo: { $regex: codigo, $options: "i" } };
@@ -57,7 +56,7 @@ const createProduct = async (req, res) => {
         console.error("Error en createProduct:", err);
         res.status(500).json({ error: err.message || "Error interno" });
     }
-}
+};
 
 const updateProduct = async (req, res) => {
     const { codigo } = req.params;
@@ -82,11 +81,97 @@ const updateProduct = async (req, res) => {
         console.error("Error en updateProduct:", err);
         return res.status(500).json({ error: err.message || "Error interno" });
     }
+};
+
+const deleteProduct = async (req, res) => {
+    const { codigo } = req.params;
+    try {
+        const deletedProduct = await Product.findOneAndDelete({ codigo: Number(codigo) });
+        if (!deletedProduct) {
+            return res.status(404).json({ message: "Producto no encontrado" });
+        }
+        res.json({ message: "Producto eliminado correctamente" });
+    } catch (err) {
+        console.error("Error en deleteProduct:", err);
+        res.status(500).json({ error: err.message || "Error interno" });
+    }
 }
+
+const searchProduct = async (req, res) => {
+    
+    const { q } = req.query;
+
+    if (!q || q.trim() === "") {
+    return res.status(400).json({ message: "El campo de búsqueda no puede estar vacío" });
+    }
+
+    try {
+    const products = await Product.find({
+        nombre: { $regex: q, $options: 'i' }
+    });
+
+    if (products.length === 0) {
+        return res.status(404).json({ message: "No products found for the given term." });
+    }
+
+    res.json({ products });
+    } catch (error) {
+    console.error("Error searching products:", error);
+    res.status(500).json({ message: "Error while searching products", error });
+}
+};
+
+const getProductByCategory = async (req, res) => {
+    const { categoria } = req.params;
+    if (!categoria || categoria.trim() === "") {
+        return res.status(400).json({message: "el campo no puede estar vacio"})
+    }
+        try {
+    const products = await Product.find({ categoria: { $regex: categoria, $options: 'i' } });
+
+    if (products.length === 0) {
+        return res.status(404).json({ message: "No se encontraron productos para esa categoría" });
+    }
+
+    res.json({ products });
+    } catch (error) {
+    console.error("Error al buscar por categoría:", error);
+    res.status(500).json({ message: "Error interno al buscar productos", error });
+}
+};
+
+const priceRange = async (req, res) => {
+    const {precio} = req.params;
+    const [minPrice, maxPrice] = precio.split('-')
+    const min = Number(minPrice);
+    const max = Number(maxPrice);
+
+    if (isNaN(min) || isNaN(max)) {
+            return res.status(400).json({ message: 'Rango de precios inválido. Usá el formato /precio/100-500' });
+    }
+    try {
+        const products = await Product.find({
+            precio: {  $gte: min, $lte: max }
+        })
+        if (products.length === 0) {
+            return res.status(404).json({ message: 'No se encontraron productos en ese rango de precio' });
+    }
+
+    res.json({ products });
+    } catch (error) {
+    console.error('Error al buscar por rango de precio:', error);
+    res.status(500).json({ message: 'Error interno al buscar productos', error });
+}
+};
+
 
 module.exports = {
     getProductAll,
     getProductById,
     createProduct,
     updateProduct,
+    deleteProduct,
+    searchProduct,
+    getProductByCategory,
+    priceRange,
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,15 +5,27 @@ const {
     getProductById,
     createProduct,
     updateProduct,
+    deleteProduct,
+    searchProduct,
+    getProductByCategory,
+    priceRange,
 
     } = require("../controllers/productController");
 
 router.get("/productos", getProductAll);
 
+router.get("/productos/buscar", searchProduct);
+
 router.get("/productos/:codigo", getProductById);
+
+router.get("/productos/categoria/:categoria", getProductByCategory)
+
+router.get("/productos/precio/:precio", priceRange)
 
 router.post("/productos", createProduct);
 
 router.put("/productos/:codigo", updateProduct);
+
+router.delete("/productos/:codigo", deleteProduct);
 
 module.exports = router;
